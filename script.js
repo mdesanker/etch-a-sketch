e509ccd85e92c142e62c13469497f82edf9cf068
Michael Desanker <mdesanker@gmail.com>
2021-09-20 22:49:00 -0400

    First attempt at JS grid. Unsuccessful

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 // script.js //
+function genGrid(num) {
+    const container = document.getElementById('container')
+    for (i = 0; i < num; i++) {
+        let row = document.createElement('div');
+        row.classList.add('row')
+        for (j = 0; j < num; j++) {
+            let cell = document.createElement('div');
+            cell.style.cssText = ""
+            cell.classList.add('grid-item');
+            cell.textContent = (i * num) + j;
+            row.appendChild(cell);
+        }
+        container.appendChild(row);
+    }
+
+}
+
+genGrid(4)
