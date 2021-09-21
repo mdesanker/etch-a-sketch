c40496e59e6810df00ba81521f3ed82655162f85
Michael Desanker <mdesanker@gmail.com>
2021-09-21 06:57:35 -0400

    Figured out how to change grid color on mouseover

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 // script.js //
 const container = document.getElementById('container');
 
+// Function to generate a grid
 function makeRows(rows, cols) {
+    // Set the position and size of the container to number of rows and cols
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
+    // Create a cell for each square unit of container
     for (i = 0; i < (rows * cols); i++) {
         let cell = document.createElement('div');
-        cell.textContent = (i + 1);
+        // cell.textContent = (i + 1);
         container.appendChild(cell).className = 'grid-item';
     }
 }
 
 makeRows(16, 16)
+
+const grids = document.querySelectorAll('#container div');
+console.log(grids)
+console.log(grids.length);
+grids.forEach((grid) => {
+    grid.addEventListener('mouseover', () => {
+        grid.style.cssText = "background-color: black"
+    })
+})
